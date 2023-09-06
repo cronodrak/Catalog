fd8dab4867bfd01b7e78e734d50b4d8bdc30de8a
Vitor Alves <vitorcarneiro778@gmail.com>
2023-09-06 19:29:08 -0300

    H2 database

---
+++ backend/src/main/java/com/cronodrak/catalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.cronodrak.catalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cronodrak.catalog.entities.Category;
+import com.cronodrak.catalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+}
