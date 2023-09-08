0cbee56d078065cd371b194be9eda3889dbabfad
Vitor Alves <vitorcarneiro778@gmail.com>
2023-09-08 19:18:24 -0300

    DTO

--- backend/src/main/java/com/cronodrak/catalog/services/CategoryService.java
+++ backend/src/main/java/com/cronodrak/catalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.cronodrak.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cronodrak.catalog.dto.CategoryDTO;
 import com.cronodrak.catalog.entities.Category;
 import com.cronodrak.catalog.repositories.CategoryRepository;
 
@@ -16,7 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 	}
 }
